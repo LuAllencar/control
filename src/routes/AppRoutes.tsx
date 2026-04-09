c2b22e9be523f4e24195fa0d82daaac10a75f8d6
lu allencar <luana.amenezes06@gmail.com>
2026-04-08 22:38:36 -0300

    add file babel.config.js

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -2,6 +2,7 @@ import AuthRoutes from "./AuthRoutes";
 import DrawerRoutes from "./DrawerRoutes";
 
 export default function AppRoutes() {
-    const isAuthenticated = true;
+    const isAuthenticated = true; 
+    // se for false, pede o login, se for true libera as partes de rotas da home
     return isAuthenticated ? <DrawerRoutes /> : <AuthRoutes />;
 }
